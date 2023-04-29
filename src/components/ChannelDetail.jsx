a41e143701f44060167bcd3019ae442ee2bf81b4
Roxsy <roxsy.dj@gmail.com>
2023-04-29 02:35:44 -0500

    estados de CahnnelDetails

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [ videos, setVideos] = useState([])
+
+  const {id} = useParams();
+  useEffect(() => {
+    fetchFromAPI(`channel? part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.item[0]));
+
+  }, [id])
+
   return (
     <div>
-      
+      {id}
     </div>
   )
 }
